2748cef111d8aa6a9c8592ec6af9b91730e5e85f
Laenz <christina.lan13@gmail.com>
2018-11-24 17:43:50 -0500

    alert

--- imageadder.js
+++ imageadder.js
@@ -1,7 +1,9 @@
-alert("Hi I'm trying!");
+<script language="javascript" type="text/javascript">
+alert('This is what an alert message looks like.');
+</script>
 
 const images = [
-        "/img/Slantd_Issue02_LooseSkins_ChristinaLan_1.png", 
+        "/img/Slantd_Issue02_LooseSkins_ChristinaLan_1.png",
         "/img/Slantd_Issue02_LooseSkins_ChristinaLan5.png",
         "/img/Slantd_Issue02_LooseSkins_ChristinaLan3.png",
         "/img/Slantd_Issue02_LooseSkins_ChristinaLan4.png",
@@ -14,26 +16,26 @@ let i = 0
 function placeImage(x, y) {
 
         const nextImage = images[i]
-        
+
         const img = document.createElement("img")
         img attribute("src", nextImage)
         img style.left = x + "px"
         img style.top = y + "px"
-        
+
         document.body.appendChild(img)
-        
+
         i = i + 1
-        
+
         if (i >= images.length) {
         i = 0
         }
-} 
-      
+}
+
 document.addEventListener("click", function (event) {
         event.preventDefault()
         placeImage(event.pageX, event.pageY)
 })
-      
+
 document.addEventListener("touchend", function (event) {
         event.preventDefault()
         placeImage(event.pageX, event.pageY)

cc0ecab46586c74092553f54694368ad73b8193e
christinalan <40611260+christinalan@users.noreply.github.com>
2018-11-24 13:00:10 -0500

    Image List

---
+++ imageadder.js
@@ -0,0 +1,6 @@
+const images = [
+        laenz.github.io/Slantd_Issue02_LooseSkins_ChristinaLan_1.png, 
+        laenz.github.io/Slantd_Issue02_LooseSkins_ChristinaLan2.png,
+        
+      
+      

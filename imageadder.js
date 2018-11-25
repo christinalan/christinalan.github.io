b4eda22da6098d1cb1df118ed95880fe4644277b
Your Name <christina.lan13@gmail.com>
2018-11-25 18:09:32 -0500

    no loop

--- imageadder.js
+++ imageadder.js
@@ -27,15 +27,12 @@ function placeImage(x, y) {
         img.setAttribute("src", nextImage)
         img.style.left = x + "px"
         img.style.top = y + "px"
-        img.style.transform = "translate(-50%, -50%) scale(0.5) rotate(" + (Math.random() * 20-10) + "deg)"
+        img.style.transform = "translate(-50%, -50%) scale(0.25) rotate(" + (Math.random() * 180-10) + "deg)"
 
         document.body.appendChild(img)
 
         i = i + 1
 
-        if (i >= images.length) {
-        i = 0
-        }
 }
 
 let mouse = 0

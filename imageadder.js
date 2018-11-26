b68a5ee4f1cb5017558b0117f5ea8cf2da996556
Your Name <christina.lan13@gmail.com>
2018-11-26 09:30:09 -0500

    another random try

--- imageadder.js
+++ imageadder.js
@@ -37,7 +37,7 @@ function placeImage(x, y) {
         img.setAttribute("src", nextImage)
         img.style.left = x + "px"
         img.style.top = y + "px"
-        img.style.transform = "translate(-50%, -50%) scale(0.25) rotate(" + (Math.random() * 180-10) + "deg)"
+        img.style.transform = "translate(-50%, -50%) scale(0.25) rotate(" + (Math.random() * 180 - 10) + "deg)"
 
         document.body.appendChild(img)
 

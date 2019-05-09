d2f08f996c20418c501e3e889421ce075c8e33bb
Your Name <christina.lan13@gmail.com>
2019-05-09 17:05:17 -0400

    fix

--- hover.js
+++ hover.js
@@ -1,13 +1,12 @@
-const slideshow = document.querySelector("section.slideshow")
-<<<<<<< HEAD
+const slideshow = document.querySelector("section.slideshow");
 const images = slideshow.querySelectorAll("img");
 
 slideshow.addEventListener("mousemove", function(event) {
   const x = event.offsetX
   const width = this.offsetWidth
 
-  const percentage = x / width
-  const imageNumber = Math.floor(percentage * images.length)
+  const percentage = x / width;
+  const imageNumber = Math.floor(percentage * images.length);
 
   images.forEach(image => {
     image.style.zIndex = 0
@@ -15,10 +14,4 @@ slideshow.addEventListener("mousemove", function(event) {
 
   images[imageNumber].style.zIndex = 1
 
-=======
-const images = slideshow.querySelectorAll("img")
-
-slideshow.addEventListener("mousemove", function(event) {
-  console.log(event)
->>>>>>> ab84d3fc2b0efd75a2b4e3d57acfd9adfd78e89a
 })

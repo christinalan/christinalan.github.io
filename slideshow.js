a0ddea8bf38e1096a87ae22317f2674d11095489
Your Name <christina.lan13@gmail.com>
2019-05-24 23:21:59 -0400

    j

--- slideshow.js
+++ slideshow.js
@@ -1,71 +1,39 @@
-var ul;
-var liItems;
-var imageWidth;
-var imageNumber;
+var slideIndex = 0;
+showSlides(); // call showslide method
 
-function init(){
+function showSlides()
+{
+    var i;
 
-    ul = document.getElementById(‘image_slider’);
-    liItems = ul.children;
-    imageNumber = liItems.length;
-    imageWidth = liItems[0].children[0].offsetWidth;
-    // set ul’s width as the total width of all images in image slider.
-    ul.style.width = parseInt(imageWidth * imageNumber) + ‘px’;
-    slider(ul);
-}
+    // get the array of divs' with classname image-sliderfade
+    var slides = document.getElementsByClassName("image-sliderfade");
 
-function slider(ul) {
-   animate({
-       delay:17,
-       duration: 3000,
-       delta:function(p){return Math.max(0, -1 + 2 * p)},
-       step:function(delta){
-           ul.style.left = ‘-’ + parseInt(currentImage * imageWidth + delta * imageWidth) + ‘px’;
-   },
-       callback:function(){
-           currentImage++;
-       // if it doesn’t slide to the last image, keep sliding
-           if(currentImage < imageNumber-1){
-               slider(ul);
-       }
-      // if current image it’s the last one, it slides back to the first one
-           else{
-               var leftPosition = (imageNumber - 1) * imageWidth;
-              // after 2 seconds, call the goBack function to slide to the first image
-               setTimeout(function(){goBack(leftPosition)},2000);
-               setTimeout(function(){slider(ul)}, 4000);
-           }
-       }
-   });
-}
+    // get the array of divs' with classname dot
+    var dots = document.getElementsByClassName("dot");
 
-function goBack(leftPosition){
-    currentImage = 0;
-    var id = setInterval(function(){
-        if(leftPosition >= 0){
-            ul.style.left = ‘-’ + parseInt(leftPosition) + ‘px’;
-            leftPosition -= imageWidth / 10;
-        }
-        else{
-            clearInterval(id);
-        }
-    } 17);
-}
+    for (i = 0; i < slides.length; i++) {
+        // initially set the display to
+        // none for every image.
+        slides[i].style.display = "none";
+    }
 
-function animate(opts){
-    var start = new Date;
-    var id = setInterval(function(){
-        var timePassed = new Date - start;
-        var progress = timePassed / opts.duration
-        if(progress > 1){
-            progress = 1;
-        }
-        var delta = opts.delta(progress);
-        opts.step(delta);
-        if (progress == 1){
-            clearInterval(id);
-           opts.callback();
-         }
-    } opts.delay || 17);
-}
-window.onload = init;
+     // increase by 1, Global variable
+    slideIndex++;
+
+     // check for boundary
+    if (slideIndex > slides.length)
+    {
+        slideIndex = 1;
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.
+                            replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    // Change image every 2 seconds
+    setTimeout(showSlides, 2000);
+} 

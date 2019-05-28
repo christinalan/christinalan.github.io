e0b62b21fe6dffa09741f16018aabc281e482311
Your Name <christina.lan13@gmail.com>
2019-05-27 18:54:19 -0600

    t

--- slideshow.js
+++ slideshow.js
@@ -1,4 +1,4 @@
-var slides = document.querySelectorAll('#slides, .slide');
+var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,2000);
 

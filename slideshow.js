df96abbd696d371f66b2fe4b47ff06903936af88
Your Name <christina.lan13@gmail.com>
2019-05-27 18:04:22 -0600

    new slideshow

--- slideshow.js
+++ slideshow.js
@@ -1,38 +1,9 @@
-var slideIndex = 0;
-showSlides(); // call showslide method
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
 
-function showSlides()
-{
-    var i;
-    // get the array of divs' with classname image-sliderfade
-    var slides = document.getElementsByClassName("image-slider");
-
-    // get the array of divs' with classname dot
-    var dots = document.getElementsByClassName("dot");
-
-    for (i = 0; i < slides.length; i++) {
-        // initially set the display to
-        // none for every image.
-        slides[i].style.display = "none";
-    }
-
-     // increase by 1, Global variable
-    slideIndex++;
-
-     // check for boundary
-    if (slideIndex > slides.length)
-    {
-        slideIndex = 1;
-    }
-
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.
-                            replace(" active", "");
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-
-    // Change image every 2 seconds
-    setTimeout(showSlides, 2000);
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
 }

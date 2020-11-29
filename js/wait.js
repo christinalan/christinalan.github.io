81de81fd2e90daf7863550419f3f0516614a7495
Christina Lan <christinalan@Christinas-MacBook-Pro.local>
2020-11-29 11:43:50 -0500

    intructions button

--- js/wait.js
+++ js/wait.js
@@ -1,3 +1,26 @@
+//variables for the Instructions window
+let modal = document.getElementById("info-modal");
+let infoButton = document.getElementById("info-button");
+//span that closes the window
+let span = document.getElementsByClassName("close")[0];
+
+window.addEventListener("load", () => {
+  //instructions window
+  infoButton.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "block";
+    }
+  };
+});
+
 let freqText = document.getElementById("freq");
 let spectrum, osc;
 let cnv;
@@ -11,34 +34,33 @@ let freq;
 function setup() {
   cnv = createCanvas(500, 500);
   background(0);
-  osc = new p5.Oscillator('sine');
+  osc = new p5.Oscillator("sine");
   fft = new p5.FFT(0.8);
-  
+
   frameRate(20);
-  
-    for (f = 50; f < 1500; f+=5) {
+
+  for (f = 50; f < 1500; f += 5) {
     freqs.push(f);
   }
 
-  cnv.mouseClicked(startAudio)
-    
+  cnv.mouseClicked(startAudio);
 }
 
 function startAudio() {
-    clicked = !clicked;
+  clicked = !clicked;
 
-    if (clicked) {
+  if (clicked) {
     osc.start();
     clear();
   } else {
     osc.stop();
-    freqText.innerHTML = freq + "Hz"
+    freqText.innerHTML = freq + "Hz";
   }
 }
 
 // function mouseClicked() {
 //   clicked = !clicked
-  
+
 //   if (clicked) {
 //     osc.start();
 //     clear();
@@ -49,8 +71,7 @@ function startAudio() {
 // }
 
 function draw() {
-
-freq = freqs[i];
+  freq = freqs[i];
   osc.freq(freq);
   i = (i + 1) % freqs.length;
 
@@ -69,10 +90,9 @@ freq = freqs[i];
 
     stroke(random(i), random(100, 200), random(50, 100), random(50, 100));
     vertex(x, y);
-    vertex(y, x, r)
-    
-     line(0, 0, x, y);
+    vertex(y, x, r);
 
+    line(0, 0, x, y);
   }
   endShape();
   pop();
